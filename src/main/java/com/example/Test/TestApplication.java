ab2e59a7ea9b2a6885f1682cb6fd9b967e1a72fd
Jekrul-Islam <sjekrul@gmail.com>
2024-11-05 12:14:59 +0600

    First Commit

---
+++ src/main/java/com/example/Test/TestApplication.java
@@ -0,0 +1,22 @@
+package com.example.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+	@GetMapping()
+	public String hello(){
+		return "hello";
+	}
+
+}
